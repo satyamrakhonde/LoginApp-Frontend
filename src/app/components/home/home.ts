68fd3759325050f8168069a1dc28faf3e920d365
satyamrakhonde <satyamrakhonde@gmail.com>
2025-11-09 22:15:42 +0530

    login component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ɵInternalFormsSharedModule } from "@angular/forms";
+import { User } from '../../models/user.model';
+import { error } from 'console';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
 
+  searchForm: FormGroup;
+  foundUser: User | null = null;
+  message: string | null = null;
+  loading: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      username: ['', Validators.required]
+    });
+  }
+
+  onSearch() {
+    this.foundUser = null;
+    this.message = null;
+
+    const username = this.searchForm.value.username?.trim();
+    if(!username) {
+      this.message = "Please enter a username to search.";
+      return;
+    } 
+
+    this.loading = true;
+    // Simulate an API call
+  //   this.userSvc.getByUsername(username).subscribe({
+  //     next: (user) => {
+  //       this.loading = false;
+  //       this.foundUser = user;
+  //     },
+  //     error: (err: HttpErrorResponse) => {
+  //       this.loading = false;
+  //       if(err.status === 404) {
+  //         this.message = "User not found.";
+  //       } else {
+  //         this.message = "Server error occurred. Please try again later.";
+  //       }
+  //     }
+  // });
+}
 }
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id?: number;
+  username: string;
+  password?: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  address: string;
+  phone: string;
+}
